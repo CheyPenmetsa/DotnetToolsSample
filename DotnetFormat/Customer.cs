dec4a0992611cd147e05b1c975622788708f992b
Chaitanya Penmetsa <CPenmetsa@americancampus.com>
2024-02-14 16:16:37 -0600

    Added some files

---
+++ DotnetFormat/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace DotnetFormat
+{
+    public class Customer
+    {
+        public string FirstName
+        { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
